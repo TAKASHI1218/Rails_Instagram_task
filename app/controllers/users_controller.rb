37339fd4cdd7068939d67bdd33b7a54bfb6e6743
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-10-12 16:38:02 +0000

    Added edit function to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,29 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報編集しました。'
+        render :edit
+      else
+        flash.now[:failed] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end
+    else
+      flash[:again] = 'idが一致しません。'
+      redirect_to :new
+    end
+  end
+
+
+
   private
 
   def user_params
