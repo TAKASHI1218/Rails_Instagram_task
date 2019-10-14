1374cc6875dd90d0a9808303d8bcda91c2a02670
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-10-14 12:37:14 +0000

    Added function to prohibit sign up without profile, update content without image and comment

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  validates :content,  presence: true, length: { minimum: 1 }
+  validates :image,  presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :profile, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
